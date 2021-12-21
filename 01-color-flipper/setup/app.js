7844c28e1c458018b9e3a3656dfd4b2be6c2981d
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-21 11:45:16 -0500

    added onclick to button with logic to change background color

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// select button by id
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// set onclick to change body background and color class div
+btn.addEventListener("click", () => {
+  // access random index on colors array
+  const randomNumber = getRandomNumber();
+  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+});
+
+// generate random number between 0 and 3
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
