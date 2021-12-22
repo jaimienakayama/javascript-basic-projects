ac2121c27af28620d2fbd07b1a7c24b8634445fb
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-22 13:14:53 -0500

    implemented basic html structure and added logic for drop down nav

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggleBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+toggleBtn.addEventListener("click", () => {
+  links.classList.contains("show-links")
+    ? links.classList.remove("show-links")
+    : links.classList.add("show-links");
+});
